e590583b4d0ad1dc166865f3a1b830edd44484fc
chicken69420 <63564747+chicken69420@users.noreply.github.com>
2021-05-03 00:11:22 +0530

    Add files via upload

--- detect/detect.js
+++ detect/detect.js
@@ -1,20 +1,74 @@
 const button = document.querySelector(".submit-button");
 const input = document.querySelector("#filer_input2");
+const img = document.querySelector(".result");
+const error = document.querySelector('.error');
 
+const url = "http://bb43b255d0d9.ngrok.io";
+let img_url;
 
-button.addEventListener('click', ()=>{
-    const files = input.files;
+button.addEventListener('click', () => {
+    const file = input.files[0];
+
+    if (!file) {
+        error.style.display = 'block';
+        return;
+    }
+
+    error.style.display = 'none';
     const formData = new FormData();
-    formData.append('file', files[0]);
 
-    fetch('https://androidapi420.herokuapp.com/api/upload/', {
-        method: 'POST',
-        body: formData,
-        mode: "no-cors"
-    })
-        .then (response => response.json())
-        .then(data => {console.log(data)})
-        .catch(error => {
-            console.error(error);
-        })
-})
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function (event) {
+        const imgElement = document.createElement("img");
+        imgElement.src = event.target.result;
+
+        imgElement.onload = function (e) {
+            const canvas = document.createElement("canvas");
+            const MAX_WIDTH = 500;
+
+            const scaleSize = MAX_WIDTH / e.target.width;
+            canvas.width = MAX_WIDTH;
+            canvas.height = e.target.height * scaleSize;
+
+            const ctx = canvas.getContext("2d");
+
+            ctx.drawImage(e.target, 0, 0, canvas.width, canvas.height);
+
+            const srcEncoded = ctx.canvas.toDataURL(e.target, "image/jpeg");
+            formData.append('file', srcEncoded);
+            formData.append('filename', "chicken.jpg");
+
+
+            fetch(url + '/api/upload/', {
+                method: 'POST',
+                body: formData,
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    img_url = url + '/' + data;
+                    img.src = img_url;
+                    img.style.display = 'block';
+                    img.style.width = '600px';
+
+
+                })
+                .catch(error => {
+                    console.log(error);
+                    error.innerText = "Some error occurred";
+                    error.style.display = 'block';
+                });
+
+
+        }
+    }
+
+
+    img.src = "load.gif";
+    img.style.display = 'block';
+    img.style.width = '200px';
+
+
+});
